1594fcf428187123fd3b4307f4ac527b2debfd7d
Luiz Filipe de Lima Gomes Santos <luizfilipelgs@gmail.com>
2023-08-12 19:57:14 -0300

    feat: adiciona spinner

--- src/components/resultScreen/ResultScreen.tsx
+++ src/components/resultScreen/ResultScreen.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import './resultScreen.css'
+import { CircularProgress } from '@mui/material'
 
 interface ResultScreenProps {
   isResetImg: boolean
   urlImgAnalise: string
+  isSend: boolean
 }
 
 const ResultScreen: React.FC<ResultScreenProps> = ({
   isResetImg,
   urlImgAnalise,
+  isSend,
 }) => {
   const [urlImg, setUrlImg] = useState(urlImgAnalise)
 
@@ -30,17 +33,23 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
 
   return (
     <div className="box-result">
-      <img
-        src={isResetImg ? '' : urlImg}
-        alt=""
-        style={{
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-        }}
-        onError={handleImageError}
-        onLoad={handleImageLoad}
-      />
+      {isSend && (!urlImgAnalise || urlImgAnalise === '') ? (
+        <div className="spinner-container">
+          <CircularProgress />
+        </div>
+      ) : (
+        <img
+          src={isResetImg ? '' : urlImg}
+          alt=""
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}
+          onError={handleImageError}
+          onLoad={handleImageLoad}
+        />
+      )}
     </div>
   )
 }
