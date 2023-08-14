b267db9c69f76765ff9f544fb43007e7acfbc20b
Luiz Filipe de Lima Gomes Santos <luizfilipelgs@gmail.com>
2023-08-14 01:20:24 -0300

    fix: converte API para ts

---
+++ src/API/index.ts
@@ -0,0 +1,36 @@
+import { client } from '@gradio/client'
+interface LabelData {
+  label: string
+}
+
+type AnaliseType = [LabelData, string, LabelData, LabelData]
+
+interface ApiResponse {
+  type: string
+  time: Date
+  data: AnaliseType
+  endpoint: string
+  fn_index: number
+}
+
+async function getAnaliseApi(
+  imgBlob: Blob,
+  modelo: string,
+): Promise<AnaliseType> {
+  try {
+    const app = await client(
+      'https://dheiver-segmento-de-angio-coronariana-v3.hf.space/',
+    )
+    const result = (await app.predict('/predict', [
+      imgBlob,
+      modelo,
+    ])) as ApiResponse
+    console.log(result.data)
+    return result.data
+  } catch (error) {
+    console.error('Erro na Requisição:', error)
+    throw error
+  }
+}
+
+export default getAnaliseApi
