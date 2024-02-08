a2bf689b04fb7d15bd61d0bd64ea7196b391658e
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-02-07 21:37:55 -0300

    feat: add repetição ao tentar acordar API

--- src/utils/wakeUpApi.ts
+++ src/utils/wakeUpApi.ts
@@ -1,16 +1,31 @@
 import imgFake from '../../src/assets/logo.png'
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export const wakeUpApi = async (getAnalise: any) => {
-  try {
-    // Fetch da imagem para obter um Blob
-    const response = await fetch(imgFake)
-    if (!response.ok) throw new Error('Falha ao carregar a imagem')
-    const imageBlob = await response.blob()
+export const wakeUpApi = async (getAnalise: (blob: Blob) => Promise<any[]>) => {
+  let attempts = 0
+  const maxAttempts = 5 // Número máximo de tentativas
+  let result: any[] = []
 
-    const result = await getAnalise(imageBlob)
-    console.log('STATUS API: online', result)
-  } catch (error) {
-    console.error('Erro ao acordar a API:', error)
+  while (attempts < maxAttempts && result.length === 0) {
+    try {
+      const response = await fetch(imgFake)
+      if (!response.ok) throw new Error('Falha ao carregar a imagem')
+      const imageBlob = await response.blob()
+
+      result = await getAnalise(imageBlob)
+      console.log('Tentativa:', attempts + 1)
+      if (result.length > 0) {
+        console.log('STATUS API: online', result)
+        break
+      }
+    } catch (error) {
+      console.error('Erro ao acordar a API:', error)
+    }
+
+    attempts += 1
+  }
+
+  if (result.length === 0) {
+    console.log('Não foi possível acordar a API após 3 tentativas.')
   }
 }
