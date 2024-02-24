29d819e9e086810b6e7cf3d15b4fe8969a033b73
luiz filipe de lima <luizfilipelgs@gmail.com>
2024-02-24 19:34:32 -0300

    chore: refatoração em andamento

--- src/pages/fractureCheck/api/api.ts
+++ src/pages/fractureCheck/api/api.ts
@@ -1,24 +1,13 @@
 import { client } from '@gradio/client'
 
-type FractureAnalysisType = [string]
-
-interface ApiResponse {
-  type: string
-  time: Date
-  data: FractureAnalysisType
-  endpoint: string
-  fn_index: number
-}
-
-async function getFractureCheck(imgBlob: Blob): Promise<FractureAnalysisType> {
+async function getFractureCheck(imgBlob: Blob): Promise<any> {
   try {
-    const app = await client(
-      'https://dheiver-deteccao-de-fraturas-em-raio-x.hf.space/',
-    )
-    const result = (await app.predict('/predict', [imgBlob])) as ApiResponse
+    const app = await client('DHEIVER/Fraturas-em-Raio-X-com-EfficientNetB3')
+    const result = await app.predict('/predict', [
+      imgBlob, // blob in 'image' Image component
+    ])
 
     console.log(result.data)
-    return result.data
   } catch (error) {
     console.error('Erro na Requisição:', error)
     throw error
